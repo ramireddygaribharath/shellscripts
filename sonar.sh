02bd34522e530c1e7519ed39d3299f98df118741
ramireddygaribharath <115775515+ramireddygaribharath@users.noreply.github.com>
2024-01-24 11:58:19 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -3,7 +3,8 @@
 cd /opt/
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
 unzip sonarqube-8.9.6.50800.zip
-amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-11-amazon-corretto
+sudo dnf install java-11-amazon-corretto-devel
 useradd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R
